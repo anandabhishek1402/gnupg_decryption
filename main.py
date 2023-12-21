b4da198a32046b29cbc395cfbcc06c5b18dfd7bf
anandabhishek1402 <154116864+anandabhishek1402@users.noreply.github.com>
2023-12-21 10:42:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,10 +14,9 @@ from flask import Flask, request
 def access_secret_version(project_id, secret_id, version_id):
     # Create the Secret Manager client
     client = secretmanager.SecretManagerServiceClient()
-    #client = secretmanager_v1.SecretManagerServiceClient()
 
     # Build the resource name of the secret version
-    name = f"projects/mm-etl-poc/secrets/gnupg-pvt/versions/latest"
+    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
 
     # Access the secret version
     response = client.access_secret_version(request={"name": name})
@@ -27,10 +26,9 @@ def access_secret_version(project_id, secret_id, version_id):
 
     return secret_data
 
-
 def read_gcs_file_to_string(bucket_name, source_blob_name):
     # Initialize the GCS client
-    storage_client = storage.Client(project='mm-etl-poc')
+    storage_client = storage.Client()
 
     # Get the bucket
     bucket = storage_client.bucket(bucket_name)
@@ -44,6 +42,7 @@ def read_gcs_file_to_string(bucket_name, source_blob_name):
     return file_content
 
 def decrypt_from_gcs(bucket_name, source_blob_name):
+    # Read encrypted data from GCS
     encrypted_data = read_gcs_file_to_string(bucket_name, source_blob_name)
     print("Inside Decrypt from GCS")
     print("Encrypted data:")
@@ -54,8 +53,8 @@ def decrypt_from_gcs(bucket_name, source_blob_name):
     encrypted_bytes = bytes(encrypted_data, 'utf-8')
 
     # Decrypt the data
-    decrypted_data = gpg.decrypt(encrypted_bytes)#, passphrase='Fossil.4')
-    print("Decruptted Data")
+    decrypted_data = gpg.decrypt(encrypted_bytes)
+    print("Decrypted Data")
     print(decrypted_data)
     # Check if the decryption was successful
     if not decrypted_data.ok:
@@ -69,23 +68,19 @@ def decrypt_from_gcs(bucket_name, source_blob_name):
     print("Decrypted data written to /tmp/decrypted_data.txt")
 
 app = Flask(__name__)
-# [END eventarc_audit_storage_server]
-# [END eventarc_http_quickstart_server]
-# app = Flask(__name__)
+
 # Initialize the GnuPG instance
 gpg = gnupg.GPG()
-# get private key from secret manager
-private_key = access_secret_version(project_id='mm-etl-poc', secret_id='gnupg-pvt', version_id='latest')
+
+# Get private key from Secret Manager
+private_key = access_secret_version(
+    project_id=os.getenv('SECRET_MANAGER_PROJECT_ID'),
+    secret_id=os.getenv('SECRET_MANAGER_SECRET_ID'),
+    version_id=os.getenv('SECRET_MANAGER_VERSION_ID')
+)
 gpg.import_keys(key_data=private_key)
-# trusting imported key is important, or it will return an empty string as encrypted data
-#gpg.trust_keys(fingerprints="81B7D836220A71C731DADAEF44803D875F3CFB84", trustlevel='TRUST_ULTIMATE')
-# set recipient which is required to decrypt
-#recipient = 'abhishekanand.78@hotmail.com'
-print("Private Key")
-#print(private_key)
 
 # [START eventarc_http_quickstart_handler]
-# [START eventarc_audit_storage_handler]
 @app.route("/", methods=["POST"])
 def index():
     print("Event received!")
@@ -100,8 +95,6 @@ def index():
 
     decrypt_from_gcs(source_bucket_name, source_blob_name)
 
-
-    # upload_stringio_to_gcs(destination_bucket_name, destination_blob_name_with_timestamp, str(decrypted_data))
     storage_client = storage.Client()
     destination_bucket_name = 'ver_dec_file'  # Replace with the actual destination bucket name
     destination_blob_name = f'decrypted/{source_blob_name}'  # Modify as needed for the destination path
@@ -113,15 +106,6 @@ def index():
 
     return 'OK', 200
 
-
-
-# [END eventarc_audit_storage_handler]
-# [END eventarc_http_quickstart_handler]
-
-
-# [START eventarc_http_quickstart_server]
-# [START eventarc_audit_storage_server]
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8080)
-# [END eventarc_audit_storage_server]
-# [END eventarc_http_quickstart_server]
+    # Run the Flask app
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get('PORT', 8080)))
