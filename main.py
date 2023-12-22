93512c40153c81805098f431d8796dfdd8694706
anandabhishek1402 <154116864+anandabhishek1402@users.noreply.github.com>
2023-12-22 09:58:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,100 +12,117 @@ from cloudevents.http import from_http
 from flask import Flask, request
 
 def access_secret_version(project_id, secret_id, version_id):
-    # Create the Secret Manager client
-    client = secretmanager.SecretManagerServiceClient()
+    try:
+        # Access the secret version
+        response = client.access_secret_version(request={"name": name})
 
-    # Build the resource name of the secret version
-    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
-
-    # Access the secret version
-    response = client.access_secret_version(request={"name": name})
-
-    # Get the secret payload as a string
-    secret_data = response.payload.data.decode("UTF-8")
-
-    return secret_data
-
-def read_gcs_file_to_string(bucket_name, source_blob_name):
-    # Initialize the GCS client
-    storage_client = storage.Client()
-
-    # Get the bucket
-    bucket = storage_client.bucket(bucket_name)
-
-    # Get the blob (file) from the bucket
-    blob = bucket.blob(source_blob_name)
-
-    # Read the file content as a string
-    file_content = blob.download_as_text()
-
-    return file_content
-
-def decrypt_from_gcs(bucket_name, source_blob_name):
-    # Read encrypted data from GCS
-    encrypted_data = read_gcs_file_to_string(bucket_name, source_blob_name)
-    print("Inside Decrypt from GCS")
-    print("Encrypted data:")
-    # Print the length of encrypted data
-    print("Length of encrypted data:", len(encrypted_data))
-
-    # Ensure encrypted_data is bytes
-    encrypted_bytes = bytes(encrypted_data, 'utf-8')
-
-    # Decrypt the data
-    decrypted_data = gpg.decrypt(encrypted_bytes)
-    print("Decrypted Data")
-    print(decrypted_data)
-    # Check if the decryption was successful
-    if not decrypted_data.ok:
-        print("Decryption failed:", decrypted_data.status)
+        # Get the secret payload as a string
+        secret_data = response.payload.data.decode("UTF-8")
+        print({"Secret Data :{}".format(secret_data)})
+        return secret_data
+    except Exception as e:
+        print(f"Error accessing secret version: {e}")
         return None
 
-    # Write the decrypted data to a file
-    with open('/tmp/decrypted_data.txt', 'wb') as f:
-        f.write(decrypted_data.data)
+def read_gcs_file_to_string(bucket_name, source_blob_name):
+    try:
+        # Initialize the GCS client
+        storage_client = storage.Client()
 
-    print("Decrypted data written to /tmp/decrypted_data.txt")
+        # Get the bucket
+        bucket = storage_client.bucket(bucket_name)
+
+        # Get the blob (file) from the bucket
+        blob = bucket.blob(source_blob_name)
+
+        # Read the file content as a string
+        file_content = blob.download_as_text()
+
+        return file_content
+    except Exception as e:
+        print(f"Error reading GCS file: {e}")
+        return None
+
+def decrypt_from_gcs(bucket_name, source_blob_name):
+    try:
+        # Read encrypted data from GCS
+        encrypted_data = read_gcs_file_to_string(bucket_name, source_blob_name)
+        print("Inside Decrypt from GCS")
+        print("Encrypted data:")
+        # Print the length of encrypted data
+        print("Length of encrypted data:", len(encrypted_data))
+
+        # Ensure encrypted_data is bytes
+        encrypted_bytes = bytes(encrypted_data, 'utf-8')
+        #pp = os.getenv(os.getenv("PASPPHRASE_SECRET_ID"))  # Use environment variable for passphrase
+        pp = access_secret_version(
+            os.getenv("PROJECT_ID"),
+            os.getenv("PASPPHRASE_SECRET_ID"),
+            "latest"
+            )
+        print("Passphrase: {}".format(pp))
+        # Decrypt the data
+        decrypted_data = gpg.decrypt(encrypted_bytes, passphrase=pp)
+        print("Decrypted Data")
+        print(decrypted_data)
+        # Check if the decryption was successful
+        if not decrypted_data.ok:
+            print("Decryption failed:", decrypted_data.status)
+            return None
+
+        # Write the decrypted data to a file
+        with open('/tmp/decrypted_data.txt', 'wb') as f:
+            f.write(decrypted_data.data)
+
+        print("Decrypted data written to /tmp/decrypted_data.txt")
+    except Exception as e:
+        print(f"Error during decryption from GCS: {e}")
+
+def upload_decrypted_to_gcs(destination_bucket, source_blob_name):
+    try:
+        storage_client = storage.Client()
+        destination_bucket_name = destination_bucket  # Use environment variable for destination bucket
+        destination_blob_name = f'decrypted/{source_blob_name}'  # Modify as needed for the destination path
+        destination_bucket = storage_client.bucket(destination_bucket_name)
+        destination_blob = destination_bucket.blob(destination_blob_name)
+    
+        with open('/tmp/decrypted_data.txt', 'rb') as f:
+            destination_blob.upload_from_file(f)
+    
+        return 'OK', 200
+    except Exception as e:
+        print(f"Error during uploading decrypted file to GCS: {e}")
+    
 
 app = Flask(__name__)
 
-# Initialize the GnuPG instance
+# Set up GnuPG instance
 gpg = gnupg.GPG()
-
-# Get private key from Secret Manager
 private_key = access_secret_version(
-    project_id=os.getenv('SECRET_MANAGER_PROJECT_ID'),
-    secret_id=os.getenv('SECRET_MANAGER_SECRET_ID'),
-    version_id=os.getenv('SECRET_MANAGER_VERSION_ID')
+    os.getenv("PROJECT_ID"),
+    os.getenv("GPG_SECRET_ID"),
+    "latest"
 )
 gpg.import_keys(key_data=private_key)
-
-# [START eventarc_http_quickstart_handler]
 @app.route("/", methods=["POST"])
 def index():
-    print("Event received!")
-    body = dict(request.json)
-    print("Event data:", body)
+    try:
+        print("Event received!")
+        body = dict(request.json)
+        print("Event data:", body)
 
-    source_bucket_name = body.get('bucket', '')
-    source_blob_name = body.get('name', '')
+        source_bucket_name = body.get('bucket', '')
+        source_blob_name = body.get('name', '')
 
-    print(f"Processing file: {source_blob_name}")
-    print(f"Bucket Name: {source_bucket_name}")
+        print(f"Processing file: {source_blob_name}")
+        print(f"Bucket Name: {source_bucket_name}")
 
-    decrypt_from_gcs(source_bucket_name, source_blob_name)
-
-    storage_client = storage.Client()
-    destination_bucket_name = 'ver_dec_file'  # Replace with the actual destination bucket name
-    destination_blob_name = f'decrypted/{source_blob_name}'  # Modify as needed for the destination path
-    destination_bucket = storage_client.bucket(destination_bucket_name)
-    destination_blob = destination_bucket.blob(destination_blob_name)
-
-    with open('/tmp/decrypted_data.txt', 'rb') as f:
-        destination_blob.upload_from_file(f)
-
-    return 'OK', 200
+        decrypt_from_gcs(source_bucket_name, source_blob_name)
+        upload_decrypted_to_gcs(os.getenv("DESTINATION_BUCKET", source_blob_name )
+        
+    except Exception as e:
+        print(f"Error during request handling: {e}")
+        return 'Internal Server Error', 500
 
 if __name__ == "__main__":
-    # Run the Flask app
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True, host="0.0.0.0", port=8080)
