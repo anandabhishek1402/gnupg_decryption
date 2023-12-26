c80949d5a0fe213bb4bc94818a1cfac84d216dd6
anandabhishek1402 <154116864+anandabhishek1402@users.noreply.github.com>
2023-12-26 18:06:49 +0530

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,2 +1,5 @@
 bind = "0.0.0.0:8080"
-workers = 3
+workers = 1
+max_requests = 1  # Limit the number of requests per worker
+worker_class = "gthread"  # Use the gthread worker class for threading support
+capture_output = False  # Disable capturing worker output
