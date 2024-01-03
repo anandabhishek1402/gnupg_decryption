aeb4677762c3b398b5804d4a1c95596d52f4bb75
anandabhishek1402 <154116864+anandabhishek1402@users.noreply.github.com>
2024-01-03 16:00:04 +0530

    Update cloud_run.tf

--- cloud_run.tf
+++ cloud_run.tf
@@ -27,6 +27,34 @@ resource "google_cloud_run_v2_service" "my_service" {
     }
     containers {
       image = "us-docker.pkg.dev/cloudrun/container/hello"
+        env {
+        name = "PROJECT_ID"
+        value = var.project_id
+      }
+      env {
+        name = "KMS_LOCATION"
+        value = google_kms_key_ring.keyring.location
+      }
+      env {
+        name = "KEY_RING"
+        value = google_kms_key_ring.keyring.name
+      }
+      env {
+        name = "CRYPTO_KEY"
+        value = google_kms_crypto_key.ver-key.name
+      }
+      env {
+        name = "GPG_SECRET_ID"
+        value = google_secret_manager_secret.secret-private-key.secret_id
+      }
+      env {
+        name = "DESTINTAION_BUCKET"
+        value = google_storage_bucket.buckets-decrypted.name
+      }
+      env {
+        name = "PASPPHRASE_SECRET_ID"
+        value = google_secret_manager_secret.passphrase.secret_id
+      }
     }
     service_account = google_service_account.pact_etl_cloudrun_sa.email
   }
